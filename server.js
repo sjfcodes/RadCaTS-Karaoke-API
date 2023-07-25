54eb37b1cc465a3b6754d3b4ffbc46144a2f594f
sjfox <sjfcodes@gmail.com>
2023-07-25 07:51:12 -0700

    startup and connect to remote db

--- server.js
+++ server.js
@@ -1,52 +1,60 @@
 // Set up the Express app
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
-const app = express();
 const cors = require("cors");
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
-app.use(cors());
-
-// Database
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/karaoke", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+const app = express();
 
-// User Routes
-const userRoutes = require("./controllers/user");
-app.use(userRoutes);
+async function main() {
+  app.use(logger("dev"));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+    );
+    next();
+  });
+  app.use(cors());
 
-// Session Routes
-const sessionRoutes = require("./controllers/session");
-app.use(sessionRoutes);
+  // Database
+  await mongoose.connect(
+    process.env.MONGO_DB_URI || "mongodb://localhost/karaoke",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
-// Song Routes
-const songRoutes = require("./controllers/song");
-app.use(songRoutes);
+  // User Routes
+  const userRoutes = require("./controllers/user");
+  app.use(userRoutes);
 
-// Mp3 Routes
-const mp3Routes = require("./controllers/mp3");
-app.use(mp3Routes);
+  // Session Routes
+  const sessionRoutes = require("./controllers/session");
+  app.use(sessionRoutes);
 
-// Lyrics Routes
-const lyricsRoutes = require("./controllers/lyrics");
-app.use(lyricsRoutes);
+  // Song Routes
+  const songRoutes = require("./controllers/song");
+  app.use(songRoutes);
 
+  // Mp3 Routes
+  const mp3Routes = require("./controllers/mp3");
+  app.use(mp3Routes);
 
-// Server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+  // Lyrics Routes
+  const lyricsRoutes = require("./controllers/lyrics");
+  app.use(lyricsRoutes);
+
+  // Server
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+}
+main();
