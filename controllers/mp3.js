019f50beb93f6ad42ccc7d724bf96af3ab233910
sjfox <sjfcodes@gmail.com>
2023-07-25 18:14:19 -0700

    fix mp3 creator

--- controllers/mp3.js
+++ controllers/mp3.js
@@ -5,81 +5,149 @@ const fs = require("fs");
 const path = require("path");
 const YoutubeMusicApi = require("youtube-music-api");
 const musicApi = new YoutubeMusicApi();
-const ytdl = require('ytdl-core');
-const ffmpeg = require('ffmpeg');
+const ytdl = require("ytdl-core");
+const ffmpeg = require("ffmpeg");
 
 //Download song
-router.post("/api/download", (req, res) => {
-  musicApi
-    .initalize() // Retrieves Innertube Config
-    .then(() => {
-      if (!req.body.name) {
-        res.json({ err: "Please enter a valid input." });
-      } else {
-        musicApi.search(`${req.body.name} original song`, "song").then((songResult) => {
-          const songName = songResult.content[0].name.toLowerCase();
-          const safeName = songName.split('/').join(' ');
-          const artistName = songResult.content[0].artist.name.toLowerCase();
+router.post("/api/download", async (req, res) => {
+  try {
+    await musicApi.initalize(); // Retrieves Innertube Config
+  } catch (err) {
+    console.error("musicApi.initialize", err);
+    res.send("musicApi.initialize", err);
+    return;
+  }
 
-          db.Song.findOne({ name: safeName, artist: artistName })
-            .then(oneSong => {
-              if (oneSong) {
-                res.send("This song already existed!")
-              } else {
-                //get and write mp4
-                const mp4FilePath = path.join(__dirname, `../music/mp4/test.mp4`);
-                const mp4 = ytdl(`http://www.youtube.com/watch?v=${songResult.content[0].videoId}`).pipe(fs.createWriteStream(mp4FilePath))
+  if (!req.body.name) {
+    res.json({ err: "Please enter a valid input." });
+    return;
+  }
 
-                mp4.on('close', () => {
-                  try {
-                    const process = new ffmpeg(path.join(__dirname, `../music/mp4/test.mp4`));
-                    process.then((video) => {
-                      //extract mp3 from mp4
-                      video.fnExtractSoundToMP3(path.join(__dirname, `../music/mp3/test.mp3`), (error, file) => {
-                        cloudinary.uploader.upload(file, {
-                          resource_type: "video",
-                          public_id: `${safeName} - ${artistName}`,
-                          folder: 'mp3',
-                          use_filename: true,
-                          chunk_size: 6000000,
-                        }, (error, result) => {
-                          if (result) {
-                            db.Song.create({
-                              name: songName,
-                              artist: artistName,
-                              mixed: result.url,
-                            }).then((newSong) => {
-                              console.log("Downloaded: ", newSong.id)
-                              res.send(newSong.id);
-                            }).catch(err => {
-                              res.status(500).send(err)
-                            })
-                          } else {
-                            res.send("The mp3 for this song is unavailable.")
-                          }
-                        });
+  let songResult;
+  try {
+    songResult = await musicApi.search(
+      `${req.body.name} original song`,
+      "song"
+    );
+    // console.log("songResult", JSON.stringify(songResult, null, 2));
+  } catch (error) {
+    console.error("musicApi.search", err);
+    res.send("musicApi.search", err);
+    return;
+  }
+
+  const index = 0;
+  let safeName;
+  let artistName;
+  try {
+    const songName = songResult.content[index].name.toLowerCase();
+    safeName = songName.split("/").join(" ");
+    artistName = songResult.content[index].artist.name.toLowerCase();
+
+    const oneSong = await db.Song.findOne({
+      name: safeName,
+      artist: artistName,
+    });
+
+    if (oneSong) {
+      res.send("This song already existed!");
+      return;
+    }
+  } catch (error) {
+    console.error("db.find", error);
+    res.send("db.find", error);
+    return;
+  }
+
+  try {
+    //get and write mp4
+    const videoId = songResult.content[index].videoId;
+    const mp4FilePath = path.join(__dirname, `../music/mp4/test.mp4`);
+    const mp4 = ytdl(`http://www.youtube.com/watch?v=${videoId}`, {
+      filter: "audio",
+    }).pipe(fs.createWriteStream(mp4FilePath));
+
+    let info = await ytdl.getInfo(videoId);
+    let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    console.log("Formats with only audio: " + audioFormats.length);
+
+    mp4.on("close", () => {
+      try {
+        new ffmpeg(path.join(__dirname, `../music/mp4/test.mp4`)).then(
+          (video) => {
+            //extract mp3 from mp4
+            video.fnExtractSoundToMP3(
+              path.join(__dirname, `../music/mp3/test.mp3`),
+              (error, file) => {
+                if (error) {
+                  console.error("video.fnExtractSoundToMP3", error);
+                  res
+                    .status(500)
+                    .send({ message: "video.fnExtractSoundToMP3", error });
+                  return;
+                }
+
+                cloudinary.uploader.upload(
+                  file,
+                  {
+                    resource_type: "video",
+                    public_id: `${safeName} - ${artistName}`,
+                    folder: "mp3",
+                    use_filename: true,
+                    chunk_size: 6000000,
+                  },
+                  (error, result) => {
+                    if (error) {
+                      console.error("cloudinary.uploader.upload.error", error);
+                      res.status(500).send({
+                        message: "cloudinary.uploader.upload.error",
+                        error,
                       });
-                    }, (err) => {
-                      console.log('Error-ffmpeg: ' + err);
-                      res.send("Error-ffmpeg", err);
-                    });
-                  } catch (err) {
-                    console.log("Error-mp4", err);
-                    res.send("Error-mp4", err);
+                      return;
+                    }
+
+                    if (!result) {
+                      console.error("cloudinary.uploader.upload.result");
+                      res.status(500).send({
+                        message: "cloudinary.uploader.upload.result missing",
+                      });
+                      return;
+                    }
+
+                    db.Song.create({
+                      name: safeName,
+                      artist: artistName,
+                      mixed: result.url,
+                    })
+                      .then((newSong) => {
+                        console.log("Downloaded: ", newSong.id);
+                        res.send(newSong.id);
+                      })
+                      .catch((error) => {
+                        console.error("db.Song.create", error);
+                        res
+                          .status(500)
+                          .send({ message: "db.Song.create", error });
+                      });
                   }
-                });
+                );
               }
-            }).catch(err => {
-              console.log("Error-Onesong", err);
-              res.send("Error-Onesong", err);
-            })
-        });
+            );
+          },
+          (err) => {
+            console.log("Error-ffmpeg: " + err);
+            res.status(500).send({ message: "Error-ffmpeg", err });
+          }
+        );
+      } catch (error) {
+        console.error("mp4.on.close", error);
+        res.status(500).send({ message: "mp4.on.close", error });
       }
-    })
-    .catch(err => {
-      console.log("Error-Search", err);
-      res.send("Error-Search", err);
-    })
+    });
+  } catch (error) {
+    console.error("ytdl.pipe", error);
+    res.status(500).send({ message: "ytdl.pipe", error });
+  }
 });
 
 module.exports = router;
